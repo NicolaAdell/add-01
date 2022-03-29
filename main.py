f8430b19ccc437bf76f0695031904d601bca8979
NicolaAdell <81983008+NicolaAdell@users.noreply.github.com>
2022-03-29 17:55:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from machine import Pin
 from time import sleep
 l1= Pin(1, Pin.OUT)
-p1= Pin(28, Pin.IN)
-p2= Pin(27, Pin.IN)
+s1= Pin(28, Pin.IN)
+s2= Pin(27, Pin.IN)
 l2= Pin(2, Pin.OUT)
 
 
 while True: 
-  if p1.value() == 1:
+  if s1.value() == 1:
     l1.on()
   else:
     l1.off()
-  while p2.value() == 1:
+  while s2.value() == 1:
     l2.on()
     sleep(.5)
-    if p2.value() == 1:
+    if s2.value() == 1:
       l2.off()
       sleep(.5)
   else:
